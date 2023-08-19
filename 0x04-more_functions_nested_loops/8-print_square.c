00b68697a34d7a0bc7cc439503a7b40300fd7efe
evarist1 <evaristchibueze@gmail.com>
2023-08-19 08:19:49 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,11 +13,12 @@ void print_square(int size)
 
 	if (size <= 0)
 		_putchar('\n');
-	for (i = 0; j < (size); i++)
+
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < (size); j++)
 		{
-		_putchar('%');
+		_putchar('#');
 		}
 		_putchar('\n');
 	}
